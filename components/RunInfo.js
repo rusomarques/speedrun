5fa0234862c5c3748d43033bb18cf22ef1ef1053
rusolm <leandromarquesp88@gmail.com>
2019-01-25 21:23:51 +0100

    feat(RunInfo): format time with moment.js

--- components/RunInfo.js
+++ components/RunInfo.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import moment from 'moment';
+import 'moment-duration-format';
+
 const RunInfo = ({ runInfo }) => (
   <div>
     <h1>Player: {runInfo.playerName}</h1>
-    <h1>Time: {runInfo.time}</h1>
+    <h1>Time: {moment.duration(runInfo.time, 'seconds').format('h:mm:ss')}</h1>
     <button className="btn btn-light">
       <a href={runInfo.videoUrl}>Watch it on YouTube</a>
     </button>
