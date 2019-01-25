8fe64dd747ff6f6579ea30e9dc90bcd437088c48
rusolm <leandromarquesp88@gmail.com>
2019-01-25 20:11:04 +0100

    feat(sagas): loadRunSaga - fetch api and dispatch loadRunSuccess with run info, or failure

--- store/sagas.js
+++ store/sagas.js
@@ -1,8 +1,13 @@
-import { put, takeLatest } from 'redux-saga/effects';
+import { put, takeLatest, all } from 'redux-saga/effects';
 import es6promise from 'es6-promise';
 import 'isomorphic-unfetch';
 
-import { actionTypes, loadGamesSuccess, loadFailure } from './actions';
+import {
+  actionTypes,
+  loadGamesSuccess,
+  loadFailure,
+  loadRunSuccess
+} from './actions';
 
 es6promise.polyfill();
 
@@ -16,8 +21,37 @@ function* loadGamesSaga() {
   }
 }
 
+function* loadRunSaga(action) {
+  try {
+    const runResponse = yield fetch(
+      `https://www.speedrun.com/api/v1/runs?game=${action.gameId}`
+    );
+    const runData = yield runResponse.json();
+    const firstRun = runData.data[0];
+    const playerResponse = yield fetch(firstRun.players[0].uri);
+    const playerData = yield playerResponse.json();
+    let playerName = 'username';
+    if (playerData.data.name) {
+      playerName = playerData.data.name;
+    } else if (playerData.data.names) {
+      playerName = playerData.data.names.international;
+    }
+    let runInfo = {
+      playerName,
+      time: firstRun.times.primary_t,
+      videoUrl: firstRun.videos.links[0].uri
+    };
+    yield put(loadRunSuccess(runInfo));
+  } catch (err) {
+    yield put(loadFailure(err));
+  }
+}
+
 function* rootSaga() {
-  yield takeLatest(actionTypes.LOAD_GAMES, loadGamesSaga);
+  yield all([
+    takeLatest(actionTypes.LOAD_GAMES, loadGamesSaga),
+    takeLatest(actionTypes.LOAD_RUN, loadRunSaga)
+  ]);
 }
 
 export default rootSaga;
