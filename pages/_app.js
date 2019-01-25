331d088d21a648578caf3530f1d54de7d81c7768
rusolm <leandromarquesp88@gmail.com>
2019-01-25 16:26:00 +0100

    feat(redux-saga): connect app with redux saga

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import App, { Container } from 'next/app';
 import React from 'react';
 import { Provider } from 'react-redux';
 import withRedux from 'next-redux-wrapper';
+import withReduxSaga from 'next-redux-saga';
 
 import createStore from './../store/store';
 
@@ -28,4 +29,4 @@ class MyApp extends App {
   }
 }
 
-export default withRedux(createStore)(MyApp);
+export default withRedux(createStore)(withReduxSaga({ async: true })(MyApp));
--- store/store.js
+++ store/store.js
@@ -1,9 +1,31 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer, { initState } from './reducer';
+import rootSaga from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const bindMiddleware = middleware => {
+  if (process.env.NODE_ENV !== 'production') {
+    const { composeWithDevTools } = require('redux-devtools-extension');
+    return composeWithDevTools(applyMiddleware(...middleware));
+  }
+  return applyMiddleware(...middleware);
+};
 
 const configureStore = (initialState = initState) => {
-  const store = createStore(rootReducer, initialState);
+  const store = createStore(
+    rootReducer,
+    initialState,
+    bindMiddleware([sagaMiddleware])
+  );
+
+  store.runSagaTask = () => {
+    store.sagaTask = sagaMiddleware.run(rootSaga);
+  };
+
+  store.runSagaTask();
 
   return store;
 };
