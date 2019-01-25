fab3e0edb8acc4b73ae74b8f4117ff0b218dc6d3
rusolm <leandromarquesp88@gmail.com>
2019-01-25 20:40:09 +0100

    feat(pages/run): render Game (logo and name)

--- pages/run.js
+++ pages/run.js
@@ -5,23 +5,33 @@ import { withRouter } from 'next/router';
 import { loadRun } from './../store/actions';
 import Layout from '../components/Layout/Layout';
 import RunInfo from '../components/RunInfo';
+import Game from '../components/Game';
 
 class Run extends Component {
   componentDidMount() {
     this.props.onLoadRun(this.props.router.query.gameId);
   }
   render() {
+    const { gameId } = this.props.router.query;
+    const game = this.props.games.find(game => game.id === gameId);
+
     let run = null;
     if (this.props.runInfo) {
       run = <RunInfo runInfo={this.props.runInfo} />;
     }
-    return <Layout>{run}</Layout>;
+    return (
+      <Layout>
+        <Game gameDetails={game} />
+        {run}
+      </Layout>
+    );
   }
 }
 
 const mapStateToProps = state => {
   return {
-    runInfo: state.run
+    runInfo: state.run,
+    games: state.games
   };
 };
 
