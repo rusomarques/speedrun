1772f3d192c0e7d7c0d3d52dba099d3e713907dc
rusolm <leandromarquesp88@gmail.com>
2019-01-25 17:24:15 +0100

    feat(Layout.js): render Navbar component

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import Navbar from './Navbar';
 
 const Layout = props => (
   <div>
@@ -12,7 +13,7 @@ const Layout = props => (
         crossorigin="anonymous"
       />
     </Head>
-    <nav />
+    <Navbar />
     <div className="container">{props.children}</div>
   </div>
 );
