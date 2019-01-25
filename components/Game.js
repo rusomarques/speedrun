ade8bd467f32b181a60855e79a5c75d853b6ddbb
rusolm <leandromarquesp88@gmail.com>
2019-01-25 20:52:43 +0100

    style(Game.js):

--- components/Game.js
+++ components/Game.js
@@ -1,15 +1,28 @@
 import React from 'react';
 
 const Game = props => (
-  <div className="card bg-light game">
-    <img
-      src={props.gameDetails.assets['cover-medium'].uri}
-      className="card-img-top"
-    />
-    <div className="card-body">
-      <h5 className="card-title">{props.gameDetails.names.international}</h5>
+  <React.Fragment>
+    <div className="card bg-light game">
+      <img
+        src={props.gameDetails.assets['cover-medium'].uri}
+        className="card-img-top"
+      />
+      <div className="card-body">
+        <h5 className="card-title">{props.gameDetails.names.international}</h5>
+      </div>
     </div>
-  </div>
+
+    <style jsx>{`
+      .game {
+        width: 25vh;
+        margin: 1em;
+        cursor: pointer;
+      }
+      img {
+        min-height: 150px;
+      }
+    `}</style>
+  </React.Fragment>
 );
 
 export default Game;
