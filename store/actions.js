928f9c3911de859d72c0791ce664ea7bdf71a302
rusolm <leandromarquesp88@gmail.com>
2019-01-25 20:05:48 +0100

    feat(actions): create LOAD_RUN & LOAD_RUN_SUCCESS

--- store/actions.js
+++ store/actions.js
@@ -1,7 +1,9 @@
 export const actionTypes = {
   LOAD_GAMES: 'LOAD_GAMES',
   LOAD_GAMES_SUCCESS: 'LOAD_GAMES_SUCCESS',
-  LOAD_FAILURE: 'LOAD_FAILURE'
+  LOAD_FAILURE: 'LOAD_FAILURE',
+  LOAD_RUN: 'LOAD_RUN',
+  LOAD_RUN_SUCCESS: 'LOAD_RUN_SUCCESS'
 };
 
 export const loadGames = () => {
@@ -19,5 +21,19 @@ export const loadFailure = error => {
   return {
     type: actionTypes.LOAD_FAILURE,
     error
+  };
+};
+
+export const loadRun = gameId => {
+  return {
+    type: actionTypes.LOAD_RUN,
+    gameId
+  };
+};
+
+export const loadRunSuccess = runInfo => {
+  return {
+    type: actionTypes.LOAD_RUN_SUCCESS,
+    runInfo
   };
 };
